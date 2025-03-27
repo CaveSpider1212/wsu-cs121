5637dddb8241786d07b500feea9fcb1eb0ee9dd5
Aabhwan Adhikary <aabhwan.adhikary@gmail.com>
2025-03-27 11:07:46 -0700

    Lab 1 commit

---
+++ Lab1Task1/MyHello.c
@@ -0,0 +1,15 @@
+/*
+* Programmer: Aabhwan Adhikary
+* Class: CptS 121, Fall 2024; Lab Section 19
+* Programming Assignment: Lab1Task1
+* Date: August 28, 2024
+* Description: This program prints out a simple "hello world" message.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello world, I am in CptS 121!");
+	
+	return 0;
+}
