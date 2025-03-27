0acec42b3b089ea64cafb49b19128755601208e8
Aabhwan Adhikary <aabhwan.adhikary@gmail.com>
2025-03-27 11:47:14 -0700

    HelloWorld commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,16 @@
+/*
+* Name: Aabhwan Adhikary
+* Date: 8/23/24
+* Class: CptS 121
+* Lab: 1
+* 
+* Description: This program displays to the screen a message that states: "Hello, CptS 121!"
+*/
+
+#include <stdio.h> // printf () - #statements are considered preprocessor directives
+
+int main(void) {
+	printf("Hello, CptS 121!\n");
+	
+	return 0; // our program ran successfully
+}
